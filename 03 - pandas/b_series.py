d816352d32c6d943765cbebd76a8495f9278330b
Aguilar Henry <henrysagq@gmail.com>
2020-07-21 03:04:06 -0500

    Deber02

---
+++ 03 - pandas/b_series.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 14 08:44:26 2020
+
+@author: henry
+"""
+
+
--- Deberes/Deber 01/mvc_exceptions.py
+++
@@ -1,6 +0,0 @@
-# mvc_exceptions.py
-class ItemAlreadyStored(Exception):
-    pass
-
-class ItemNotStored(Exception):
-    pass
